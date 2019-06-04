9a8e4d9dc180b71c5afe59142180afd176e927de
Maoqiang Zheng <soymikey@163.com>
2019-06-04 10:01:33 +0800

    修改baseUrl 图片可以获取的到

--- src/components/adminComponent/headTop.vue
+++ src/components/adminComponent/headTop.vue
@@ -16,6 +16,7 @@
         :src="imgBaseUrl + adminInfo.avatar"
         class="avator"
       >
+
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item command="home">首页</el-dropdown-item>
         <el-dropdown-item command="signout">退出</el-dropdown-item>
--- src/config/env.js
+++ src/config/env.js
@@ -1,18 +1,15 @@
-/**
- * 配置编译环境和线上环境之间的切换
- *
- * baseUrl: 域名地址
- * routerMode: 路由模式
- * imgBaseUrl: 图片所在域名地址
- *
- */
+//  配置编译环境和线上环境之间的切换
 
-let baseUrl = 'http://localhost:8001'
+//  baseUrl: 域名地址
+//  routerMode: 路由模式
+//  imgBaseUrl: 图片所在域名地址
+
+let baseUrl = '//localhost:8001'
 let routerMode = 'hash'
 let imgBaseUrl = ''
 
 if (process.env.NODE_ENV === 'development') {
-  imgBaseUrl = '/img/'
+  imgBaseUrl = 'http://localhost:8001/img/'
 } else if (process.env.NODE_ENV === 'production') {
   baseUrl = '//elm.cangdu.org'
   imgBaseUrl = '//elm.cangdu.org/img/'
